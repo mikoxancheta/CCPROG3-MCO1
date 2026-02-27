63fbff4103763ec856b321535c3a30198dacec04
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2026-02-28 02:40:51 +0800

    Update Fertilizer.java

--- CCPROG3-MCO1/src/com/verdantsun/Fertilizer.java
+++ CCPROG3-MCO1/src/com/verdantsun/Fertilizer.java
@@ -1,4 +1,36 @@
 package com.verdantsun;
 
 public class Fertilizer {
+
+    private String name;
+    private int price;
+    private int effectDays;
+
+    public Fertilizer(String name, int price, int effectDays) {
+        this.name = name;
+        this.price = price;
+        this.effectDays = effectDays;
+    }
+
+    public void decreaseEffect() {
+        if (this.effectDays > 0) {
+            this.effectDays--;
+        }
+    }
+
+    public boolean isExpired() {
+        return this.effectDays <= 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public boolean isActive() {
+        return this.effectDays > 0;
+    }
 }
