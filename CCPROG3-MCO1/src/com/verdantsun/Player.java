8e2f4c4b712aecbaf168ea1e72184fb9c49f20d3
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2026-03-06 16:30:40 +0800

    Update Player.java

--- CCPROG3-MCO1/src/com/verdantsun/Player.java
+++ CCPROG3-MCO1/src/com/verdantsun/Player.java
@@ -1,4 +1,36 @@
 package com.verdantsun;
 
 public class Player {
+
+    private String name;
+    private int savings;
+
+    public Player(String name) {
+        this.name = name;
+        this.savings = 1000;
+    }
+
+    public void addSavings(int amount) {
+        this.savings += amount;
+    }
+
+    public boolean deductSavings(int amount) {
+        if (this.savings >= amount) {
+            this.savings -=amount;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean canAfford(int amount) {
+        return this.savings >= amount;
+    }
+
+    public int getSavings() {
+        return this.savings;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
