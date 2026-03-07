33e57509920b8d8c78988b23c525cecc827c5f0e
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2026-03-07 12:10:12 +0800

    Update FertilizerLoader.java

--- CCPROG3-MCO1/src/com/verdantsun/FertilizerLoader.java
+++ CCPROG3-MCO1/src/com/verdantsun/FertilizerLoader.java
@@ -58,26 +58,3 @@ public class FertilizerLoader {
         return fertilizers;
     }
 }
-                for (String attr : attributes) {
-
-                    String[] pair = attr.split(":");
-                    String attrKey = pair[0].replace("\"", "").trim();
-                    String attrValue = pair[1].replace("\"", "").trim();
-
-                    switch (attrKey) {
-                        case "name": name = attrValue; break;
-                        case "price": price = Integer.parseInt(attrValue); break;
-                        case "days": days = Integer.parseInt(attrValue); break;
-                    }
-                }
-
-                fertilizers.put(key, new Fertilizer(name, price, days));
-            }
-
-        } catch (IOException e) {
-            System.out.println("Error loading fertilizers file.");
-        }
-
-        return fertilizers;
-    }
-}
